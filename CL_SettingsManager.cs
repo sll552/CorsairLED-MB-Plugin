88258352670329c0f986be85a6e681f851e11a53
Stefan Lengauer <slengauer522@gmail.com>
2018-01-05 23:53:08 +0100

    - add setters - write default device to config - actually use the stringconverters - only return a section if it contains the wanted key

--- CL_SettingsManager.cs
+++ CL_SettingsManager.cs
@@ -11,13 +11,24 @@ namespace MusicBeePlugin
   /// </summary>
   class ClSettingsManager
   {
+    private const string GlobalSection = "Global";
     private readonly string _configFile;
     private readonly Configuration _config;
+    private string _defaultDev;
 
     /// <summary>
     /// The device to use as provider for default values for colors
     /// </summary>
-    public string Defaultdev { get; set; }
+    public string Defaultdev
+    {
+      get => _defaultDev;
+      set
+      {
+        _defaultDev = value;
+        var section = _config[GlobalSection];
+        section["DefaultDevice"].StringValue = _defaultDev;
+      }
+    }
 
     /// <summary>
     /// Create a new Configuration Manager for the given config file. If the file exists the config is read, otherwise a new config will be created.
@@ -26,6 +37,8 @@ namespace MusicBeePlugin
     public ClSettingsManager(string config)
     {
       _configFile = config ?? throw new ArgumentNullException();
+      Configuration.RegisterTypeStringConverter(new ClColorStringConverter());
+      Configuration.RegisterTypeStringConverter(new ClColoringModeStringConverter());
       _config = File.Exists(_configFile) ? Configuration.LoadFromFile(_configFile) : new Configuration();
     }
 
@@ -44,8 +57,20 @@ namespace MusicBeePlugin
     /// <returns>The configured color, or RED if none can be found</returns>
     public Color GetPrimaryColor(string device)
     {
-      var section = GetSectionForDev(device);
-      return section?["PrimaryColor"].GetValue<Color>() ?? Color.Red;
+      const string key = "PrimaryColor";
+      var section = GetSectionForDev(device, key);
+      return section?[key].GetValue<Color>() ?? Color.Red;
+    }
+
+    /// <summary>
+    /// Set the primary effect color for the device
+    /// </summary>
+    /// <param name="device">The name of the device</param>
+    /// <param name="color">The color to set</param>
+    public void SetPrimaryColor(string device, Color color)
+    {
+      var section =_config[device];
+      section["PrimaryColor"].SetValue(color);
     }
 
     /// <summary>
@@ -55,8 +80,20 @@ namespace MusicBeePlugin
     /// <returns>The configured color, or BLACK if none can be found</returns>
     public Color GetBackgroundColor(string device)
     {
-      var section = GetSectionForDev(device);
-      return section?["BackgroundColor"].GetValue<Color>() ?? Color.Black;
+      const string key = "BackgroundColor";
+      var section = GetSectionForDev(device, key);
+      return section?[key].GetValue<Color>() ?? Color.Black;
+    }
+
+    /// <summary>
+    /// Set the background color for the device
+    /// </summary>
+    /// <param name="device">The name of the device</param>
+    /// <param name="color">The color to set</param>
+    public void SetBackgroundColor(string device, Color color)
+    {
+      var section = _config[device];
+      section["BackgroundColor"].SetValue(color);
     }
 
     /// <summary>
@@ -66,18 +103,43 @@ namespace MusicBeePlugin
     /// <returns>The configured coloring mode, or ColoringMode.Solid if none can be found</returns>
     public ClSpectrumBrushFactory.ColoringMode GetColoringMode(string device)
     {
-      var section = GetSectionForDev(device);
-      return section?["ColoringMode"].GetValue<ClSpectrumBrushFactory.ColoringMode>() ?? ClSpectrumBrushFactory.ColoringMode.Solid;
+      const string key = "ColoringMode";
+      var section = GetSectionForDev(device, key);
+      return section?[key].GetValue<ClSpectrumBrushFactory.ColoringMode>() ?? ClSpectrumBrushFactory.ColoringMode.Solid;
+    }
+
+    /// <summary>
+    /// Set the coloring mode for the device
+    /// </summary>
+    /// <param name="device">The name of the device</param>
+    /// <param name="cm">The coloring mode to set</param>
+    public void SetColoringMode(string device, ClSpectrumBrushFactory.ColoringMode cm)
+    {
+      var section = _config[device];
+      section["ColoringMode"].SetValue(cm);
     }
 
     /// <summary>
     /// Get the setting for the k95 platinum lightbar
     /// </summary>
     /// <param name="device">The name of the device</param>
-    /// <returns>the configured lightbar setting, if the device is not a k95 platinum false is returned</returns>
+    /// <returns>The configured lightbar setting, if the device is not a k95 platinum false is returned</returns>
     public bool GetLightbarProgress(string device)
     {
       return device == "K95 RGB PLATINUM" && _config[device]["LightbarProgress"].BoolValue;
+    }
+
+    /// <summary>
+    /// Set the setting for the k95 platinum lightbar
+    /// </summary>
+    /// <param name="device">The name of the device</param>
+    /// <param name="lbprog">The lightbar setting</param>
+    public void SetLightbarProgress(string device, bool lbprog)
+    {
+      if (device == "K95 RGB PLATINUM")
+      {
+        _config[device]["LightbarProgress"].BoolValue = lbprog;
+      }
     }
 
     /// <summary>
@@ -85,10 +147,11 @@ namespace MusicBeePlugin
     /// If none of these Sections exist null is returned
     /// </summary>
     /// <param name="device">The name of the requested device</param>
+    /// <param name="key">The requested config key</param>
     /// <returns>The <see cref="Section"/> to use for the device or null if no default device is configured</returns>
-    private Section GetSectionForDev(string device)
+    private Section GetSectionForDev(string device, string key)
     {
-      if (_config.Contains(device))
+      if (_config.Contains(device) && _config[device].Contains(key))
       {
         return _config[device];
       }
