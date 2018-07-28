6ca36fd721bfa16729459a413a74c1444319ad32
Stefan Lengauer <slengauer522@gmail.com>
2018-07-28 15:03:07 +0200

    - fix combobox showing unsupported effects

--- UI/SettingsWindow.cs
+++ UI/SettingsWindow.cs
@@ -81,6 +81,11 @@ namespace MusicBeePlugin.UI
       dataGridView1.DataSource = null;
       dataGridView1.Columns.Clear();
       dataGridView1.Rows.Clear();
+
+      dataGridView1.CellValueChanged += DataGridView1OnCellValueChanged;
+      dataGridView1.CellMouseUp += DataGridView1OnCellMouseUp;
+      dataGridView1.CellClick += DataGridView1OnCellClick;
+
       dataGridView1.Refresh();
       _binding.Clear();
       _binding.DataSource = null;
@@ -122,20 +127,14 @@ namespace MusicBeePlugin.UI
         Name = "Enabled"
       });
 
-      dataGridView1.CellValueChanged += DataGridView1OnCellValueChanged;
-      dataGridView1.CellMouseUp += DataGridView1OnCellMouseUp;
-      dataGridView1.DataBindingComplete += DataGridView1OnDataBindingComplete;
-
     }
 
     // Set the combobox datasource for the supported effects for each device
-    private void DataGridView1OnDataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs dataGridViewBindingCompleteEventArgs)
+    private void DataGridView1OnCellClick(object sender, DataGridViewCellEventArgs e)
     {
-      foreach (DataGridViewRow row in dataGridView1.Rows)
+      if (e.ColumnIndex == dataGridView1.Columns["Effect"]?.Index)
       {
-        if (row.Cells.Count <= dataGridView1.Columns["Effect"]?.Index) continue;
-        if (row.Cells.Count <= dataGridView1.Columns["Name"]?.Index) continue;
-
+        var row = dataGridView1.Rows[e.RowIndex];
         var tmp = (DataGridViewComboBoxCell)row.Cells[dataGridView1.Columns["Effect"].Index];
         var devname = row.Cells[dataGridView1.Columns["Name"].Index].Value.ToString();
         var effects = _devices.First(device => device.DeviceName == devname).GetSupportedEffects();
